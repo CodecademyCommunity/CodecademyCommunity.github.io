6bb81b65e0efbb23f62386612a5bd77cb02fe13f
Victoria <68347113+Victoria-DR@users.noreply.github.com>
2021-12-28 16:34:46 -0500

    Remove hompage property

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route path='CodecademyCommunity.github.io' element={<Home />} />
+        <Route path='/' element={<Home />} />
       </Routes>
       <Footer />
     </BrowserRouter>
