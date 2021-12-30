ce60dc3d75e23e4ff1346f3426343c36bcfdafb3
aedwardg <aedwardg1@gmail.com>
2021-12-30 14:32:18 -0800

    update stylesheet architecture

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './assets/sass/bulma_styles.scss';
-import './assets/css/style.scss'
 
 ReactDOM.render(
   <React.StrictMode>
