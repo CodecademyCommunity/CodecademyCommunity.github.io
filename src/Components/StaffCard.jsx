7500882854194fc1c7d7c7d6e910fd9e44c62eb1
Lyall Stewart <me@lyallstewart.com>
2022-02-05 15:00:07 +0000

    Readded modals with React support

--- src/Components/StaffCard.jsx
+++ src/Components/StaffCard.jsx
@@ -1,10 +1,24 @@
-export const StaffCard = (props) => {
-    const {name, pfp, role} = props;
+import {useState} from 'react';
 
+export const StaffCard = (props) => {
+    const {name, pfp, role, bio} = props;
+    const [isActive, setIsActive] = useState(false);
+    
     return (
-        <div className="staff-card has-background-white">
-            <img alt={`${name}'s profile`} className="profile-picture" src={pfp}/>
-            <p className={`name ${role}`}>{name}</p>
-        </div>
+        <>
+            <div className="staff-card has-background-white">
+                <img alt={`${name}'s profile`} className="profile-picture" src={pfp}/>
+                <p className={`name ${role}`}>{name}</p>
+                <button onClick={() => setIsActive(!isActive)} className="bio-button">See Bio</button>
+            </div>
+
+            <div className={`${isActive ? "is-active" : ""} modal`}>
+                <div onClick={() => setIsActive(false)} className="modal-background"></div>
+                <div className="modal-content">
+                    <p>{bio}</p>
+                </div>
+                <button onClick={() => setIsActive(false)} className="modal-close is-large" aria-label="close"></button>
+            </div>
+        </>
     );
 }
