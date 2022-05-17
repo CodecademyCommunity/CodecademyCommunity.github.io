9295dea402f98436cefbf0f20dd007ad6c694fbf
Ying <97061798+yingmo55@users.noreply.github.com>
2022-05-17 16:07:37 -0400

    Adjust h1 spacing. Add method to sort resource links
    - ResourceLink sorting method suggested by MikeJ

--- src/Views/Resources.jsx
+++ src/Views/Resources.jsx
@@ -5,22 +5,29 @@ export const Resources = () => {
   return (
     <main className="has-text-centered">
       <div className="container p-2">
-        <h1 className="is-size-1 mt-6">Helpful Resources</h1>
+        <h1 className="is-size-1 is-size-2-mobile mb-3">Helpful Resources</h1>
         <p className="mb-6">
           A list of resources surrounding the topic of programming and
           programming-related skills
         </p>
       </div>
       <section className="container has-background-white mb-6 p-6">
-        {data.resources.map((resource, index) => (
-          <ResourceLink
-            title={resource.title}
-            url={resource.url}
-            author={resource.author}
-            tags={resource.tags}
-            key={index}
-          />
-        ))}
+        {data.resources
+          .sort((a, b) => {
+            return a.title.toUpperCase().replace('THE', '').trim() <
+              b.title.toUpperCase().replace('THE', '').trim()
+              ? -1
+              : 1;
+          })
+          .map((resource, index) => (
+            <ResourceLink
+              title={resource.title}
+              url={resource.url}
+              author={resource.author}
+              tags={resource.tags}
+              key={index}
+            />
+          ))}
       </section>
     </main>
   );
