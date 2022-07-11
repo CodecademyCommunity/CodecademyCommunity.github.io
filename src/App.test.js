7b663e615a5a00b0bf25b11cdec72d0e092f6aa3
aedwardg <aedwardg1@gmail.com>
2022-07-10 18:09:27 -0700

    Refactor window scroll
    Move scroll to top functionality into its own component.
    
    Pass that new component in as child to App component to avoid the need
    to mock window.scroll in every test where it might get called.

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,6 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import App from './App';
 
-window.scroll = jest.fn();
-
 describe('home page', () => {
   it('renders the Home page', () => {
     render(<App />);
